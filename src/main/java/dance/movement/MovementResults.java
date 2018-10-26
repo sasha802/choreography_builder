db4b8e6b126b070ca0801172a8208926d2bf792b
Sasha <oleksanova@gmail.com>
2018-10-25 22:53:23 -0500

    Getting java onjec from dance movement service.

---
+++ src/main/java/dance/movement/MovementResults.java
@@ -0,0 +1,75 @@
+package dance.movement;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.annotation.Generated;
+
+@Generated("com.robohorse.robopojogenerator")
+public class MovementResults{
+
+	@JsonProperty("danceName")
+	private String danceName;
+
+	@JsonProperty("deleted")
+	private int deleted;
+
+	@JsonProperty("danceMovement")
+	private String danceMovement;
+
+	@JsonProperty("dancePosition")
+	private String dancePosition;
+
+	@JsonProperty("id")
+	private int id;
+
+	public void setDanceName(String danceName){
+		this.danceName = danceName;
+	}
+
+	public String getDanceName(){
+		return danceName;
+	}
+
+	public void setDeleted(int deleted){
+		this.deleted = deleted;
+	}
+
+	public int getDeleted(){
+		return deleted;
+	}
+
+	public void setDanceMovement(String danceMovement){
+		this.danceMovement = danceMovement;
+	}
+
+	public String getDanceMovement(){
+		return danceMovement;
+	}
+
+	public void setDancePosition(String dancePosition){
+		this.dancePosition = dancePosition;
+	}
+
+	public String getDancePosition(){
+		return dancePosition;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"MovementResults{" + 
+			"danceName = '" + danceName + '\'' + 
+			",deleted = '" + deleted + '\'' + 
+			",danceMovement = '" + danceMovement + '\'' + 
+			",dancePosition = '" + dancePosition + '\'' + 
+			",id = '" + id + '\'' + 
+			"}";
+		}
+}
--- src/test/java/dance/builder/entity/DanceMovementTest.java
+++ src/test/java/dance/builder/entity/DanceMovementTest.java
@@ -1,15 +1,32 @@
 package dance.builder.entity;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import dance.builder.persistence.GenericDAO;
+import dance.movement.MovementResults;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DanceMovementTest {
 
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    InputStream input = null;
+    Properties properties = new Properties();
     GenericDAO<DanceMovement> genericDAODanceMovement;
 
     @BeforeEach
@@ -22,7 +39,29 @@ class DanceMovementTest {
     void getByPropertyEqualSuccess() {
 
         List<DanceMovement> list = genericDAODanceMovement.getByPropertyEqual("danceName", "Waltz");
+        logger.info(list.get(0).getDanceMovement());
         assertEquals(1, list.size());
+
+    }
+
+    @Test
+    public void testDanceMovementJson() {
+
+        String danceName = "Waltz";
+        Client client = ClientBuilder.newClient();
+
+        try {
+            WebTarget target = client.target("http://localhost:8080/choreographybuilder/service/danceMovement/" + danceName);
+            String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            MovementResults results = mapper.readValue(response, MovementResults.class);
+            assertEquals("Slow Waltz is danced using a normal ballroom-style closed position dance hold.", results.getDancePosition());
+
+        } catch (IOException ioException) {
+            logger.error(ioException);
+        }
 
     }
 }
